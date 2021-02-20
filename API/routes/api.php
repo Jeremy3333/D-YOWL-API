8b1f2e01631faa559a05ee94b04450649a13b3b7
jeremy3333 <jeremy.simond@epitech.digital>
2021-02-20 16:35:59 +0100

    save

--- API/routes/api.php
+++ API/routes/api.php
@@ -8,9 +8,9 @@ use App\Http\Controllers\UserController;
 Route::get('post',[Postcontroller::class,'getposts']);
 Route::get('post/{id}',[Postcontroller::class,'getpost']);
 Route::post('post',[Postcontroller::class,'storepost']);
-Route::get('user',[UserController::class,'get']);
-Route::get('user/{id}',[UserController::class,'getuser']);
-Route::post('user',[UserController::class,'storeuser']);
+Route::get('users',[UserController::class,'get']);
+Route::get('users/{id}',[UserController::class,'getuser']);
+Route::post('users',[UserController::class,'storeuser']);
 Route::post('test',[Postcontroller::class,'test']);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
